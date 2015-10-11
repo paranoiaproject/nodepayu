bcce2bf64e306053456d86ee4f6bdf2df49f1cca
hwclass <harderworking.class@gmail.com>
2015-10-12 02:26:53 +0300

    Edited the content of Request model to make post for payment and loyalty inquiry

--- src/models/Request.js
+++ src/models/Request.js
@@ -1,53 +1,99 @@
 'use strict';
 
+var request = require('request');
+
+var PaymentRequestBuilder = require('PaymentRequestBuilder');
+var LoyaltyInquiryRequestBuilder = require('LoyaltyInquiryRequestBuilder');
+
 /**
  * Request is the model for returning result from payU
- * @param {string} error
- * @param {string} response
- * @param {string} body
+ * @param {Config} config
  */
 var Request = (function () {
 
-  return function (error, response, body) {
+  return function (config) {
 
     var constructor = new Object();
 
-    constructor.error = error;
-
-    constructor.response = response;
-
-    constructor.body = body;
+    constructor.config = config;
 
     return {
 
       /**
-       * getError() is a getter method to the returning error message
+       * createPaymentRequestBuilder() is a getter method to the returning response
        *
-       * @param {function} callback
-       * @return error
+       * @param {Config} config
+       * @return PaymentRequestBuilder
       */
-      getError : function (callback) {
-        callback(constructor.error);
+      createPaymentRequestBuilder : function (config) {
+        return new PaymentRequestBuilder(config);
       },
 
       /**
-       * getResponse() is a getter method to the returning response
+       * createLoyaltyInquiryRequestBuilder() is a getter method to the returning response
        *
-       * @param {function} callback
-       * @return response
+       * @param {Config} config
+       * @return LoyaltyInquiryRequestBuilder
       */
-      getResponse : function (callback) {
-        callback(constructor.response);
+      createLoyaltyInquiryRequestBuilder : function (config) {
+        return new LoyaltyInquiryRequestBuilder(config);
       },
 
       /**
-       * getBody() is a getter method to the returning body of the response
+       * post() is a method to make some requests for payment and loyalty processes
        *
+       * @param {string} endpointUrl
        * @param {function} callback
-       * @return body
+       * @param {function} errorCallback
       */
-      getBody : function (callback) {
-        callback(constructor.body);
+      post : function (endpointUrl, callback, errorCallback) {
+        try {
+          var result;
+          request(endpointUrl, function (error, response, body) {
+            if (error) {
+              errorCallback(error.message);
+            } else {
+              result = body;
+            }
+          });
+          callback(result);
+        } catch (e) {
+          errorCallback(e);
+        }
+      },
+
+      /**
+       * makePayment() is a method to make some requests for payment
+       *
+       * @param {Config} config
+       * @param {function} callback
+       * @param {function} errorCallback
+      */
+      makePayment : function (config, callback, errorCallback) {
+        var response;
+        this.post(config.getPaymentEndpointUrl(), function (data) {
+          response = data;
+        }, function (error) {
+          errorCallback(error.message);
+        });
+        callback(response);
+      },
+
+      /**
+       * makeLoyaltyInquiry() is a method to make some requests for loyalty
+       *
+       * @param {Config} config
+       * @param {function} callback
+       * @param {function} errorCallback
+      */
+      makeLoyaltyInquiry : function (config, callback, errorCallback) {
+        var response;
+        this.post(config.getLoyaltyInquiryEndPointUrl(), function (data) {
+          response = data;
+        }, function (error) {
+          errorCallback(error.message);
+        });
+        callback(response);
       }
 
     }
