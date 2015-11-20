de70afcd86e570fcc5dccdf181d66facfd290d52
hwclass <harderworking.class@gmail.com>
2015-11-20 19:13:11 +0200

    Added the client file for the application

---
+++ src/models/NodePayu.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ *  NodePayu the model for the current application as client
+ * @noparam
+ */
+var NodePayu = (function () {
+
+  return function () {
+
+    var constructor = new Object();
+
+    return {
+
+      /**
+       * initialMethod() is a getter method
+       *
+       * @noparam
+       * @return {object} null
+      */
+      initialMethod : function () {
+        return null;
+      }
+
+    }
+
+  }
+
+})();
