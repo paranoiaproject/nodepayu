f1b18b850ca302e58f713d32433069affe9362d7
hwclass <harderworking.class@gmail.com>
2016-06-13 00:05:07 +0300

    moved the old version into a folder versioned and started the new one with stamps

--- src/models/Config.js
+++ src/models/Config.js
@@ -1,128 +1,65 @@
-'use strict';
+const builder = require('stampit');
 
-/**
- * Config is the model for configuration variables
- * @param {string} merchantId
- * @param {string} secretKey
- * @param {string} paymentEndpointUrl
- * @param {string} loyaltyInquiryEndPointUrl
- * @param {string} paymentReturnPointUrl
- */
-var Config = (function () {
+const defaults = builder().refs({
+	merchantId: '',
+	secretKey: '',
+	paymentEndpointUrl: '',
+	loyaltyInquiryEndPointUrl: '',
+	paymentReturnPointUrl: ''
+});
 
-    return function build (merchantId, secretKey, paymentEndpointUrl, loyaltyInquiryEndPointUrl, paymentReturnPointUrl) {
-        
-      var constructor = new Object();
-      
-      constructor.merchantId = merchantId;
-      
-      constructor.secretKey = secretKey;
-      
-      constructor.paymentEndpointUrl = paymentEndpointUrl;
-      
-      constructor.loyaltyInquiryEndPointUrl = loyaltyInquiryEndPointUrl;
-      
-      constructor.paymentReturnPointUrl = paymentReturnPointUrl;
-        
-      return {
-        
-        /**
-          * getMerchantId() is a getter method to fetch merchant id
-          *
-          * @noparam
-          * @return merchantId
-         */
-         getMerchantId : function () {
-           return constructor.merchantId;
-         },
-         
-         /**
-          * getSecretKey() is a getter method to fetch secret key
-          *
-          * @noparam
-          * @return secretKey
-         */
-         getSecretKey : function () {
-           return constructor.secretKey;
-         },
-         
-         /**
-          * getSecretKey() is a getter method to fetch endpoint url for payment
-          *
-          * @noparam
-          * @return paymentEndpointUrl
-         */
-         getPaymentEndpointUrl : function () {
-           return constructor.paymentEndpointUrl;
-         },
-         
-         /**
-          * getLoyaltyInquiryEndPointUrl() is a getter method to fetch endpoint url for loyalty inquiry
-          *
-          * @noparam
-          * @return loyaltyInquiryEndPointUrl
-         */
-         getLoyaltyInquiryEndPointUrl : function () {
-           return constructor.loyaltyInquiryEndPointUrl;
-         },
-         
-         /**
-          * getPaymentReturnPointUrl() is a getter method to fetch point url for payment return
-          *
-          * @noparam
-          * @return paymentReturnPointUrl
-         */
-         getPaymentReturnPointUrl : function () {
-           return constructor.paymentReturnPointUrl;
-         },
-         
-         /**
-          * setMerchantId() is a setter method to update merchant id
-          *
-          * @param {string} merchantId
-         */
-         setMerchantId : function (merchantId) {
-           constructor.merchantId = merchantId;
-         },
-         
-         /**
-          * setMerchantId() is a setter method to update secret key
-          *
-          * @param {string} secretKey
-         */
-         setSecretKey : function (secretKey) {
-           constructor.secretKey = secretKey;
-         },
-         
-         /**
-          * setPaymentEndpointUrl() is a setter method to update endpoint url for payment
-          *
-          * @param {string} paymentEndpointUrl
-         */
-         setPaymentEndpointUrl : function (paymentEndpointUrl) {
-           constructor.paymentEndpointUrl = paymentEndpointUrl;
-         },
-         
-         /**
-          * setLoyaltyInquiryEndPointUrl() is a setter method to update endpoint url for loyalty inquiry
-          *
-          * @param {string} loyaltyInquiryEndPointUrl
-         */
-         setLoyaltyInquiryEndPointUrl : function (loyaltyInquiryEndPointUrl) {
-           constructor.loyaltyInquiryEndPointUrl = loyaltyInquiryEndPointUrl;
-         },
-         
-         /**
-          * setPaymentReturnPointUrl() is a setter method to update point url for payment return
-          *
-          * @param {string} paymentReturnPointUrl
-         */
-         setPaymentReturnPointUrl : function (paymentReturnPointUrl) {
-           constructor.paymentReturnPointUrl = paymentReturnPointUrl;
-         } 
-          
-      }      
+/*
+const config = builder().init(function() {
+  
+  this.getMerchantId = () => {
+    return merchantId;
+  };
 
+  this.setMerchantId = (merchantId) => {
+    merchantId = merchantId;
+  };
+
+  this.getSecretKey = () => {
+    return secretKey;
+  };
+
+  this.getPaymentEndpointUrl = () => {
+    return paymentEndpointUrl;
+  };
+
+  this.getLoyaltyInquiryEndPointUrl = () => {
+    return loyaltyInquiryEndPointUrl;
+  };
+
+  this.getPaymentReturnPointUrl = () => {
+    return paymentReturnPointUrl;
+  };
+
+});
+*/
+
+const config = builder({
+  methods: {
+    getMerchantId() {
+      this.members[member.name] = member;
+      return this;
+    },
+    getMember(name) {
+      return this.members[name];
     }
+  },
+  refs: {
+    merchantId: '',
+    secretKey: '',
+		paymentEndpointUrl: '',
+		loyaltyInquiryEndPointUrl: '',
+		paymentReturnPointUrl: ''
+  }
+});
 
-})();
+//module.exports = Config;
+
+const bar = builder.compose(defaults, config);
+
+console.dir(bar);
+bar.getMerchantId();
